2ab45e5f5b774a29e5ed18aa104f8afd35727a6f
Adam Muana <adammuanawork@gmail.com>
2023-09-04 11:07:56 +0800

    Created sign up component, form input component, button component

---
+++ src/components/sign-up-form/SignUpForm.component.js
@@ -0,0 +1,109 @@
+import { useState } from 'react'
+
+import { 
+    createAuthUserWithEmailAndPassword, 
+    createUserDocumentFromAuth
+} from '../../utils/firebase/Firebase.utils'
+
+import FormInput from '../form-input/FormInput.component'
+import Button from '../button/Button.component'
+import './SignUpForm.styles.scss'
+
+const defaultFormFields = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
+
+const SignUpForm = () => {
+    const [formFields, setFormFields] = useState(defaultFormFields)
+    const { displayName, email, password, confirmPassword } = formFields
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields)
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+
+        setFormFields({ ...formFields, [name]: value })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        if(password !== confirmPassword) {
+            alert('Password Did not match');
+            return
+        }
+        try {
+            const { user } = await createAuthUserWithEmailAndPassword(email, password)
+            await createUserDocumentFromAuth(user, { displayName })
+            resetFormFields()
+        } catch (err) {
+            if(err.code === 'auth/email-already-in-use') {
+                alert('Cannot create user, email already exist.')
+            }
+            console.log('User creation error:', err);
+        }
+            // console.log('Form Submitted');
+            
+            //const userDocRef = await createUserDocumentFromEmailAndPassword(form, displayName);
+    }
+
+    return (
+        <div className='sign-up-container'>
+            <h2>Don't have an account?</h2>
+            <span>Sign Up with your email and password</span>
+            <form onSubmit={handleSubmit}>
+                <FormInput
+                    label='Display Name'
+                    inputOptions={{
+                        type:'text',
+                        onChange:handleChange,
+                        name:'displayName',
+                        value:displayName,
+                        required:true
+                    }}
+                />
+                <FormInput
+                    label='Email'
+                    inputOptions={{
+                        type:'email',
+                        onChange:handleChange,
+                        name:'email',
+                        value:email,
+                        required:true
+                    }}
+                />
+                <FormInput
+                    label='Password'
+                    inputOptions={{
+                        type:'password',
+                        onChange:handleChange,
+                        name:'password',
+                        value:password,
+                        minLength:'6',
+                        required:true
+                    }}
+                />
+                <FormInput
+                    label='Confirm Password'
+                    inputOptions={{
+                        type:'password',
+                        onChange:handleChange,
+                        name:'confirmPassword',
+                        value:confirmPassword,
+                        minLength:'6',
+                        required:true
+                    }}
+
+                />
+                <Button type='submit'>Sign Up</Button>
+            </form>
+        </div>
+    )
+}
+
+export default SignUpForm
--- src/utils/firebase/Firebase.utils.js
+++ src/utils/firebase/Firebase.utils.js
@@ -3,7 +3,8 @@ import {
     getAuth, 
     signInWithRedirect, 
     signInWithPopup, 
-    GoogleAuthProvider 
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword
 } from 'firebase/auth'
 
 import {
@@ -12,6 +13,7 @@ import {
     getDoc,
     setDoc
 } from 'firebase/firestore'
+import { async } from 'q'
 
 const firebaseConfig = {
     apiKey: 'AIzaSyCfkO6DEiDFwSH6dyFW_6VjeFlEgWswMLE',
@@ -24,25 +26,23 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig)
 
-const provider = new GoogleAuthProvider()
+const googleProvider = new GoogleAuthProvider()
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
     prompt: 'select_account'
 })
 
 export const auth = getAuth()
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 
 export const db = getFirestore()
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation) => {
+    if(!userAuth) return
     const userDocRef = doc(db, 'users', userAuth.uid)
-    console.log(userAuth);
-    console.log(userDocRef);
 
     const userSnapshot = await getDoc(userDocRef)
-    console.log(userSnapshot);
-    console.log(userSnapshot.exists());
 
     if(!userSnapshot.exists()) {
         const { displayName, email } = userAuth
@@ -52,7 +52,8 @@ export const createUserDocumentFromAuth = async (userAuth) => {
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation
             })
         } catch (err) {
             console.log('error creating the user', err.message);
@@ -60,8 +61,10 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     }
 
     return userDocRef
+}
 
-    //if user data does not exists
-    //if user data exists
-    //return userDocRef
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return
+    
+    return await createUserWithEmailAndPassword(auth, email, password)
 }
