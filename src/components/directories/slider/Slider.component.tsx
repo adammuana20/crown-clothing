033e5b556659ae00d0f3555a62b600727d56122d
Adam Clint Muana <Adam.Muana@entaingroup.com>
2023-12-03 14:08:22 +0800

    Fixing slider

--- src/components/directories/slider/Slider.component.tsx
+++ src/components/directories/slider/Slider.component.tsx
@@ -1,96 +1,101 @@
-import { FC, useEffect, useState, useRef } from "react"
+import { FC, useEffect, useState, useRef, useCallback } from "react"
 
-import { AutoSliderContainer, SliderSlide, ImgSlider, SliderSlides, DotContainer, Dot } from "./Slider.styles"
+import { SliderContainer, ImgSlider, DotContainer, Dot, ImgContainer, SliderContainerOverflow } from "./Slider.styles"
 
 
 type SliderProps = {
-    imageUrls: string[];
+    sliders: { imageUrl: string; title: string; }[];
 }
 
-const Slider: FC<SliderProps> = ({ imageUrls }) => {
+const Slider: FC<SliderProps> = ({ sliders }) => {
     const [currentSlide, setCurrentSlide] = useState(0)
-    const interval = 3000
+    const [startX, setStartX] = useState<number | null>(null);
+    const [isDragging, setIsDragging] = useState(false);
 
-    const sliderRef = useRef<HTMLDivElement>(null);
-    let startX: number | undefined;
+    const timerRef = useRef<NodeJS.Timeout | null>(null)
+
+    const goToNext = useCallback(() => {
+        const isLastSlide = currentSlide === sliders.length -1
+        const newIndex = isLastSlide ?  0 : currentSlide + 1
+        setCurrentSlide(newIndex)
+    }, [currentSlide, sliders.length])
 
     useEffect(() => {
-        const handleTouchStart = (e: TouchEvent | MouseEvent) => {
-            startX = 'touches' in e ? (e as TouchEvent).touches[0].clientX : (e as MouseEvent).clientX;
-        };
-
-        const handleTouchMove = (e: TouchEvent | MouseEvent) => {
-        const moveX = 'touches' in e ? (e as TouchEvent).touches[0].clientX : (e as MouseEvent).clientX;
-            if(startX !== undefined) {
-            const deltaX = moveX - startX;
-
-            // Implement your swipe logic based on deltaX
-            if (deltaX < 0) {
-                // Swipe to the right
-                setCurrentSlide((prevSlide) => (prevSlide + 1) % imageUrls.length);
-            } else if (deltaX > 0) {
-                // Swipe to the left
-                setCurrentSlide((prevSlide) => (prevSlide - 1 + imageUrls.length) % imageUrls.length);
-            }
-    
-            // Reset startX to handle the next swipe
-            startX = undefined;
-            }
-        };
-
-        const sliderElement = sliderRef.current as HTMLElement | null;
-
-        if (sliderElement) {
-            sliderElement.addEventListener('touchstart', handleTouchStart);
-            sliderElement.addEventListener('touchmove', handleTouchMove);
-
-            sliderElement.addEventListener('mousedown', handleTouchStart);
-            sliderElement.addEventListener('mousemove', handleTouchMove);
+        if (timerRef.current && !isDragging) {
+            clearTimeout(timerRef.current);
         }
-
+    
+        if (!isDragging) {
+            timerRef.current = setTimeout(() => {
+                goToNext();
+            }, 2000);
+        }
+    
         return () => {
-            // Cleanup event listeners when the component unmounts
-            if (sliderElement) {
-                sliderElement.removeEventListener('touchstart', handleTouchStart);
-                sliderElement.removeEventListener('touchmove', handleTouchMove);
-
-                sliderElement.removeEventListener('mousedown', handleTouchStart);
-                sliderElement.removeEventListener('mousemove', handleTouchMove);
+            if (timerRef.current) {
+                clearTimeout(timerRef.current);
             }
         };
-    }, []);
+    }, [goToNext, isDragging])
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCurrentSlide((prevSlide) => (prevSlide + 1) % imageUrls.length)
-        }, interval)
-
-        return () => clearInterval(intervalId);
-    }, [imageUrls.length, interval])
 
     const goToSlide = (index: number) => {
         setCurrentSlide(index)
     }
 
+    const handleTouchStart = (e: React.MouseEvent | React.TouchEvent) => {
+        e.preventDefault()
+        setStartX("touches" in e ? e.touches[0].clientX : e.clientX);
+    }
+
+    const handleTouchMove = (e: React.TouchEvent | React.MouseEvent) => {
+        if (!startX || isDragging) return;
+    
+        const currentX = "touches" in e ? e.touches[0].clientX : e.clientX;
+        const deltaX = currentX - startX;
+    
+        if (deltaX > 50) {
+          // Dragged to the right
+          setCurrentSlide((prev) => (prev === 0 ? sliders.length - 1 : prev - 1));
+          setIsDragging(true);
+        } else if (deltaX < -50) {
+          // Dragged to the left
+          setCurrentSlide((prev) => (prev === sliders.length - 1 ? 0 : prev + 1));
+          setIsDragging(true);
+        }
+    };
+
+    const handleTouchEnd = () => {
+        setIsDragging(false);
+        setStartX(null);
+    };
+
     return (
-        <AutoSliderContainer ref={sliderRef}>
+        <>
             <DotContainer>
-                {imageUrls.map((_, index) => (
+                { sliders.map((_, index) => (
                     <Dot key={index} onClick={() => goToSlide(index)} $isActive={currentSlide === index} />
                 ))}
             </DotContainer>
-            <SliderSlides style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
-            { imageUrls.map((imageUrl, idx) => (
-                <SliderSlide
-                    key={idx}
-                    onClick={() => goToSlide(idx)}
+            <SliderContainerOverflow>
+                <SliderContainer
+                    $currentSlide={currentSlide}
+                    onMouseDown={handleTouchStart}
+                    onTouchStart={handleTouchStart}
+                    onMouseMove={handleTouchMove}
+                    onTouchMove={handleTouchMove}
+                    onMouseUp={handleTouchEnd}
+                    onTouchEnd={handleTouchEnd}
                 >
-                    <ImgSlider src={imageUrl} alt={`Slide ${idx + 1}`} />
-                </SliderSlide>
-                ))
-            }
-            </SliderSlides>
-        </AutoSliderContainer>
+                    { sliders.map((slider, idx) => (
+                        <ImgContainer key={idx} >
+                            <ImgSlider src={slider.imageUrl} alt={slider.title} />
+                        </ImgContainer>
+                        ))
+                    }
+                </SliderContainer>
+            </SliderContainerOverflow>
+        </>
     )
 }
 
--- src/components/directories/slider/Slider.styles.tsx
+++ src/components/directories/slider/Slider.styles.tsx
@@ -1,38 +1,9 @@
 import styled, { css } from "styled-components";
 
-
 type SliderProps = {
-    $isActive: boolean;
+    $isActive?: boolean;
+    $currentSlide?: number;
 }
-
-export const OutletContainer = styled.div`
-    
-`
-
-export const SliderActive = css`
-    display: block;
-    opacity: 1;
-`
-
-export const AutoSliderContainer = styled.div`
-    position: relative;
-    overflow: hidden;
-`
-
-export const SliderSlides = styled.div`
-    display: flex;
-    transition: transform 0.5s ease-in-out;
-`
-
-export const SliderSlide = styled.div`
-    flex: 0 0 100%;
-    height: 35rem;
-`
-
-export const ImgSlider = styled.img`
-    width: 100%;
-    height: 100%;
-`
 
 export const DotContainer = styled.div`
     position: absolute;
@@ -41,7 +12,7 @@ export const DotContainer = styled.div`
     transform: translateX(-50%);
     display: flex;
     align-items: center;
-    gap: 8px;
+    gap: 10px;
     z-index: 2;
 `
 
@@ -64,3 +35,26 @@ export const Dot = styled.div<SliderProps>`
         background-color: #fff;
     }
 `;
+
+export const SliderContainerOverflow = styled.div`
+    overflow: hidden;
+    height: 100%;
+`
+
+export const SliderContainer = styled.div<SliderProps>`
+    height: 100%;
+    position: relative;
+    transform: translateX(-${({ $currentSlide }) => $currentSlide && $currentSlide * 100}%);
+    transition: transform 0.5s ease-in-out;
+    display: flex;    
+`
+
+export const ImgContainer = styled.div`
+    flex: 0 0 100%;
+`
+
+export const ImgSlider = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`
